100d102fe2f64ab6771099692dfa90d01d4c9659
Shkaffez <shishkov-i@mail.com>
2021-12-18 21:53:36 +0300

    hw

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-import { of, from } from 'rxjs';
+import { from, defer} from 'rxjs';
+import axios from 'axios';
+import { map } from 'rxjs/operators'
 
-const o = of(5) // Promise.resolve(5)
 
-o.subscribe({
+const observer = {  
   next: (value: any) => console.log('Next:', value),
   complete: () => console.log('Complete!'),
   error: (error) => console.log('Error!', error)
-})
+};
+
+const githubApi$ = defer(() => 
+    axios.get(`https://api.github.com/search/repositories?q=javascript&per_page=1`)     
+  ).pipe(
+    map((response) => response.data.items)        
+  );
+  
+githubApi$.subscribe(observer);
+
+
+const gitlabApi$ = from(
+  axios.get(`https://gitlab.com/api/v4/projects?search=typescript&per_page=3`)     
+).pipe(
+  map((response) => response.data)    
+);
+
+gitlabApi$.subscribe(observer);
